14fc5815670114f7a5ebd7ced158b0715913a8e7
RITU DASH <91211437+dashdritu@users.noreply.github.com>
2023-12-26 17:32:38 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SignUp from './components/auth/SignUp'; import Login from components/auth从osin;
+import SignUp from './components/auth/SignUp'; 
+import Login from './components/auth/Login';
 import Header from './components/common/Header';
 import Spinner from './components/common/Spinner';
-import AllRestaurants from ./components/customer/AllRestaurants'; import BookTable from components/customer/BookTable';
+import AllRestaurants from './components/customer/AllRestaurants'; 
+import BookTable from './components/customer/BookTable';
 import Dashboard from './components/customer/Dashboard';
-import MyBookings from components/customer/MyBookings;
-import ReservationRequests from /components/manager/ReservationRequests';
-import RegisterRestaurant from /components/manager/RegisterRestaurant;
+import MyBookings from './components/customer/MyBookings';
 import Api from './components/services/Api';
 import './styles/index.css';
 
@@ -29,8 +29,7 @@ return(
  <Route path="/all-restaurants"><AllRestaurants /></Route>
 <Route path="/book-table/:restaurantId"><BookTable /></Route>
 <Route path="/dashboard"> <Dashboard /></Route>
-<Route path="/my-bookings"><MyBookings /></Route> <Route path='/reservation-requests'><ReservationRequests /></Route>
-<Route path="/register-restaurant"><RegisterRestaurant /></Route>
+<Route path="/my-bookings"><MyBookings /></Route>
 <Route path="/spinner"><Spinner /></Route>
 <Route path="/api"><Api /></Route>
 
